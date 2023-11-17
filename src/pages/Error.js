df1cc115bef3d6f3a412c905762fb104722a29fc
joyhyonie <joyhyonie@gmail.com>
2023-11-17 13:49:32 +0900

    ✨ Feat: Error 페이지 완료

--- src/pages/Error.js
+++ src/pages/Error.js
@@ -1,10 +1,27 @@
 import React from "react";
+import CommonCSS from "../css/common.module.css";
+import blocks from "../../public/images/blocks.png";
+import { useNavigate } from "react-router-dom";
 function Error() {
+  const navigate = useNavigate();
+
+  const goToStart = () => {
+    // localStorage.clear(); // 처음으로: 기존의 data 모두 clear
+    localStorage.removeItem("autoMode");
+    localStorage.removeItem("profile");
+    localStorage.removeItem("nickname");
+    localStorage.removeItem("first");
+    navigate("/");
+  };
+
   return (
-    <>
-      <div>유효하지않은 url입니당!</div>
-      <button>메인으로</button>
-    </>
+    <div className={CommonCSS.error}>
+      <img className={CommonCSS.blocks} src={blocks} alt="blocks" />
+      <div className={CommonCSS.errorBox}>
+        <p>유효하지않은 URL입니다! 😶‍</p>
+        <button onClick={goToStart}>돌아가기</button>
+      </div>
+    </div>
   );
 }
 
